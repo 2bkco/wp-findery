7f96c2be31c211c21a8b5b3e8bcd68e496c7465c
Mike Fowler <mike@findery.com>
2012-12-04 15:29:29 -0800

    Restructure plugin to use an embed handler. Makes embedding easier.

--- findery.php
+++ findery.php
@@ -1,30 +1,67 @@
 <?php
 /*
-Plugin Name: Findery
-Plugin URI: http://findery.com
-Description: A shortcode plugin for embedding content from Findery in Wordpress blogs
-Version: 0.1
-Author: Findery
-Author URI: http://findery.com
-License: MIT License (http://opensource.org/licenses/MIT)
-*/
+ * Plugin Name: Findery
+ * Plugin URI: http://findery.com
+ * Description: A shortcode plugin for embedding content from Findery in Wordpress blogs
+ * Version: 0.1
+ * Author: Findery
+ * Author URI: http://findery.com
+ *
+ * License: GPLv2 or later
+ *
+ * Copyright (C) 2012 Findery
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
-function findery_embed( $atts ) {
+/**
+ * Don't call this file directly.
+ */
+if ( ! class_exists( 'WP' ) ) {
+  die();
+}
 
-  extract( shortcode_atts( array (
-    'w' => '500',
-    'h' => '400'
-  ), $atts ));
+final class Findery_Embed {
 
-  return sprintf('<iframe src="%s" width="%s" height="%s"></iframe>',
-                 preg_replace('/([^\/:])\//', '${1}/embed/', $atts[0], 1),
-                 $atts['width'],
-                 $atts['height']);
+  private static $embed_base = '<iframe src="%s" width="%s" height="%s"></iframe>';
+  private static $default_width = '500';
+  private static $default_height = '400';
+
+  public static function init() {
+    wp_embed_register_handler('findery', '#https:\/\/findery.com\/(notes|sets|[_a-z0-9]+$|[-_a-z0-9]+\/notes)(?:\/)?([-_a-z0-9]+)?#i', array( __CLASS__, 'findery_embed_handler') );
+  }
+
+  public static function findery_embed_handler( $matches, $attr, $url, $rawattr ) {
+
+    if ( ! empty( $rawattr['width'] ) && ! empty( $rawattr['height'] ) ) {
+      $width  = $rawattr['width'];
+      $height = $rawattr['height'];
+    } else {
+      list( $width, $height ) = wp_expand_dimensions( self::$default_width, self::$default_height, $attr['width'], $attr['height'] );
+    }
+
+    return sprintf(self::$embed_base,
+                   preg_replace('/([^\/:])\//', '${1}/embed/', $url, 1),
+                   $width,
+                   $height);
+  }
 
 }
 
-function register_findery_shortcode() {
-  add_shortcode( 'findery', 'findery_embed' );
-}
+/**
+ * Initialize plugin
+ */
 
-add_action( 'init', 'register_findery_shortcode' );
+add_action( 'plugins_loaded', array( 'Findery_Embed', 'init' ) );
